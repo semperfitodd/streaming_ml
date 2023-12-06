6052fbd3aa5d25b533d70932c0f115cd9f1f929f
Jake Schlottag <jschlottag.1@gmail.com>
2023-12-06 14:04:28 -0500

    add response block, create clear button, move buttons

--- terraform/site/twitter-insights/src/App.js
+++ terraform/site/twitter-insights/src/App.js
@@ -1,12 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import ParticlesBg from 'particles-bg'
 import SearchTopic from './components/search-topic/search-topic.component';
-
-const config = {
-};
-
-
 
 function App() {
   return (
--- terraform/site/twitter-insights/src/components/search-topic/search-topic.component.jsx
+++ terraform/site/twitter-insights/src/components/search-topic/search-topic.component.jsx
@@ -4,44 +4,58 @@ import './search-topic.styles.css';
 
 const SearchTopic = () => {
     const [inputValue, setInputValue] = useState('');
+    const [responseText, setResponseText] = useState('');
     const apiEndpoint = 'https://mecji43lle.execute-api.us-east-1.amazonaws.com/producer';
 
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
     };
 
-    const handleSubmit = async () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault(); // Prevents the default form submission behavior (clearing the console)
         try {
             const response = await fetch(apiEndpoint, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ search_query: inputValue }),
-                mode: 'no-cors'
+                body: JSON.stringify({ search_query: inputValue })
             });
 
             if (response.ok) {
                 const jsonResponse = await response.json();
-                console.log('Response:', jsonResponse);
+                setResponseText(JSON.stringify(jsonResponse, null, 2));
             } else {
-                console.error('Request failed with status:', response.status);
+                setResponseText(`Request failed with status: ${response.status}`);
             }
         } catch (error) {
-            console.error('Request failed:', error);
+            setResponseText(`Request failed: ${error}`);
         }
+    };
+
+    const handleClear = () => {
+        setInputValue('');
+        setResponseText('');
     };
 
     return (
         <div className="container">
             <div className="row justify-content-center align-items-center vh-100">
                 <div className="col-md-6">
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className="input-group mb-3">
                             <input type="text" className="form-control" value={inputValue} onChange={handleInputChange} placeholder="Twitter Topic" />
-                            <button className="btn btn-info" onClick={handleSubmit}>Go</button>
+                        </div>
+                        <div className='d-flex justify-content-center mb-3'>
+                            <button className="btn btn-success me-3 btn-equal-width" type="submit">Go</button>
+                            <button className="btn btn-danger btn-equal-width" type="button" onClick={handleClear}>Clear</button>
                         </div>
                     </form>
+                    {responseText && (
+                        <pre className="response-output">
+                            {responseText}
+                        </pre>
+                    )}
                 </div>
             </div>
         </div>
